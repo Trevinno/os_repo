b75e30a6f05d785776f0eabc9d57833261e24dfd
Ricardo <josericardo.trdz@gmail.com>
2019-11-19 21:52:41 -0600

    el nuevo commit

--- benchmark/mmult.sh
+++ benchmark/mmult.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
-declare -A matrix
-num_rows=4
-num_columns=5
-
-for ((i=1;i<=num_rows;i++)) do
-    for ((j=1;j<=num_columns;j++)) do
-        matrix[$i,$j]=($RANDOM % 100)
-    done
-done
-
-f1="%$((${#num_rows}+1))s"
-f2=" %9s"
-
-printf "$f1" ''
-for ((i=1;i<=num_rows;i++)) do
-    printf "$f2" $i
-done
-echo
-
-for ((j=1;j<=num_columns;j++)) do
-    printf "$f1" $j
-    for ((i=1;i<=num_rows;i++)) do
-        printf "$f2" ${matrix[$i,$j]}
-    done
-    echo
+echo $'Tamano,Tiempo' >> salida.txt
+for i in 4 8 16 32 64 128 256 512 1024 2048 4096
+do
+   echo $i, $(./mmult $i) >> salida.txt
 done
